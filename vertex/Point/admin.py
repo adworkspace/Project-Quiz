5e7932548a49c04deb6591a46bc1f784429cabea
Adarsh Dash <99327144+adworkspace@users.noreply.github.com>
2023-01-17 01:07:52 +0530

    Vertex-1.1.2

--- vertex/Point/admin.py
+++ vertex/Point/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from Point.models import Question,Category
+from Point.models import Question,Category,Result
 
 # Register your models here.
 admin.site.register(Question)
 admin.site.register(Category)
+admin.site.register(Result)
 
 
--- vertex/Point/models.py
+++ vertex/Point/models.py
@@ -1,20 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 #Creating models here
-
+# Category model
 class Category(models.Model):
     sl_no=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=100,default="No Name")
+    number_of_questions=models.IntegerField(default=10)
+    time=models.IntegerField(help_text="duration of the quiz in minutes.",default=1)
+    required_score=models.IntegerField(help_text="required score to pass the quiz.",default=60)
     cat=models.CharField(max_length=50)
     cat_img=models.ImageField(upload_to="static/category_media",blank=True)
     parent=models.ForeignKey('self',on_delete=models.CASCADE,null=True,blank=True)
-
     def __str__(self):
         return str(self.sl_no) + " " +self.cat
 
     def get_questions(self):
         return self.question_set.all()
 
+    class Meta:
+        verbose_name_plural='Categories'
 
+
+    
+
+#Question model
 class Question(models.Model):
     q_no=models.AutoField(primary_key=True)
     quest=models.TextField(max_length=500)
@@ -29,3 +39,12 @@ class Question(models.Model):
 
     def __str__(self):
         return "Question "+str(self.q_no)
+
+# Result model
+class Result(models.Model):
+    quiz=models.ForeignKey(Category,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    score=models.FloatField()
+
+    def __str__(self):
+        return str(self.pk)
